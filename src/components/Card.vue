8d4833e1aa27b6ec0b730f8f57b88cfe7cf4d330
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-03-07 19:01:19 +0100

    FEAT image into card added

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,11 @@
 <template>
 
     <li class="card-result">
+        <img :src="getImage()" alt="">
         <p>Titolo: {{ result.title }}</p>
         <p>Titolo originale: {{ result.original_title }}</p>
         <p>Lingua originale: {{ result.original_language }}</p>
-        <p>Lingua originale: {{ result.vote_average }}</p>
+        <p>Voto: {{ result.vote_average }}</p>
     </li>
 
 
@@ -16,6 +17,15 @@ export default{
         result: {
             type: Array,
             required: true
+        }
+    },
+    methods:{
+        getImage(){
+        const base_url = 'https://image.tmdb.org/t/p/';
+        const file_size = 'w500';
+        const file_path = this.result.poster_path;
+        const poster_path = base_url + file_size + file_path;
+        return poster_path;
         }
     }
 
